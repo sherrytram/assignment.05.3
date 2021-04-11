4b5467d773fc714308e9875389c435851ead30c1
Sherry Tram <sherrymtram@gmail.com>
2021-04-11 14:12:29 -0700

    reusable comps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const reuseComponet = 
+  <div className="comptext">
+    <br></br>
+    <h2>Reusable Component</h2>
+    <p>This is a reusable component. Lorem ipsum dolor 
+      sit amet, consectetur adipiscing elit, sed do eiusmod 
+      tempor incididunt ut labore et dolore magna aliqua. 
+      </p>
+      <br></br>
+    
+
+  </div>;
+
+class App extends Component {
+  render() {
+    return <div>
+      <div className="heading"><h1>Sherry Tram - Web Dev Assginment #05</h1></div>
+      <br></br>
+      <div className="compsection">
+      <div className="compbox">{reuseComponet}</div>
+      <div className="compbox">{reuseComponet}</div>
+      <div className="compbox">{reuseComponet}</div>
+      <div className="compbox">{reuseComponet}</div>
+      <div className="compbox">{reuseComponet}</div>
+      <div className="compbox">{reuseComponet}</div>
+    </div>;
     </div>
-  );
+    
+    
+
+  }
 }
 
 export default App;
